0ffe6531206d0b02676a55b23ca91cd360c3ecc6
Frederic Delorme <frederic.delorme@gmail.com>
2025-05-07 11:50:18 +0200

    Create the project

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,10 +1,12 @@
 public class App{
-  public App(){}
+  public App(){
+    System.out.println("Welcome to JDemo !");    
+  }
   public static void main(String[] args){
     App app = new App();
     app.run(args);
   }
   public void run(String[] args){
-    System.ou.println(Hello World!);
+    System.out.println("Hello World!");
   }
 }
