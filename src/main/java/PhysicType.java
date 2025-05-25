31deb275b5ad033d0d5a0236abff408b8ec83259
Frederic Delorme <frederic.delorme@gmail.com>
2025-05-25 12:21:10 +0200

    upgrade build script

--- src/main/java/PhysicType.java
+++
@@ -1,5 +0,0 @@
-public enum PhysicType {
-    STATIC,
-    DYNAMIC,
-    NONE;
-}
--- src/main/java/Utils.java
+++
@@ -1,7 +0,0 @@
-import java.awt.*;
-
-public class Utils {
-    public static Color setAlpha(Color c, float v) {
-        return new Color(c.getRed(), c.getGreen(), c.getBlue(), (int) (v * 255));
-    }
-}
